30eb4db3cd506a74bc0af30eb9748f7626d0bf21
Eddy Hernandez <edward.d.hernandez@gmail.com>
2015-11-24 17:48:14 -0800

    better support for collapsing/showing different images

--- content_script.js
+++ content_script.js
@@ -1,20 +1,43 @@
 (function() {
   'use strict';
 
-  var links = document.querySelectorAll('.comment-body a[href$="png"]:not(.gh-a-to-img)');
-  var linkImages = document.querySelectorAll('.comment-body a.gh-a-to-img');
+  var imgExts = ['png','jpg','gif','jpeg','svg'];
+  var ghImageHost = 'https://camo.githubusercontent.com';
+  var linkSelector = [];
+  var linkedImages = document.querySelectorAll('.comment-body a img');
+  var links;
 
-  if(links.length > 0){
-    for (var i = 0; i < links.length; i++) {
-      var text = links[i].textContent;
-      links[i].className = links[i].className + " gh-a-to-img";
-      links[i].setAttribute('data-content', text);
-      links[i].innerHTML = '<img class="gh-img-to-a" src="' + links[i].href + '">';
-    }
+  for (var i = 0; i < imgExts.length; i++) {
+    linkSelector.push('.comment-body a[href$="' + imgExts[i] + '"]');
+  }
+
+  links = document.querySelectorAll(linkSelector.join(',') + ', .comment-body a[href^="' + ghImageHost + '"]');
+
+  if(linkedImages.length > 0){
+    console.log(1);
+    replaceLinkedImages(links);
   } else {
-    for (var i = 0; i < linkImages.length; i++) {
-      linkImages[i].className = linkImages[i].className.replace('gh-a-to-img','');
-      linkImages[i].innerHTML = linkImages[i].getAttribute('data-content');
+    console.log(2);
+    replaceLinks(links);
+  }
+
+  function replaceLinkedImages(links) {
+    for (var i = 0; i < links.length; i++) {
+      var link = links[i];
+      var img = link.getElementsByTagName('img')[0];
+      link.className += ' gh-a-rm-img';
+      link.innerHTML = img.getAttribute('alt') || '(image)';
+      link.setAttribute('data-content', link.innerHTML);
+    }
+  }
+
+  function replaceLinks(links) {
+    console.log(links);
+    for (var i = 0; i < links.length; i++) {
+      var link = links[i];
+      var altText = link.textContent;
+      link.className = link.className.replace('gh-a-rm-img','');
+      link.innerHTML = '<img class="gh-img-to-a" src="' + link.href + '" alt="' + altText + '">';
     }
   }
 }());
